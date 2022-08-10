19934c2e89036a879aa3f8768cb15b8b4812dac2
Bosong Zhang <bosongzhang@gmail.com>
2022-08-10 12:39:12 -0400

    Create creat_bxz.sh

---
+++ creat_bxz.sh
@@ -0,0 +1,16 @@
+#!/bin/bash 
+
+###################################################################
+#Script Name : create_cg37.sh
+#Description : Create a conda env for common use
+#Args        : 
+#Author      : Chenggong Wang 
+#Email       : c.wang@princeton.edu  
+###################################################################
+echo $SHELL
+conda create -y -n cg37 python=3.7 numpy=1.21 xarray=0.20 dask
+conda activate cg37 
+conda install -y -c conda-forge  esmpy=7.1.0 xesmf=0.3  netCDF4 h5py h5netcdf 
+conda install -y -c conda-forge cartopy=0.20 matplotlib 
+conda install -y -c conda-forge nc-time-axis ipympl scipy scikit-learn line_profiler numba ipykernel 
+python -m ipykernel install --user --name cg37 --display-name "cg37" # Register the kernel for jupyter
